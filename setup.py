29dc6eaf6d197659950adefc8cc38ab05d21ac65
Peter Wilton <peterrwilton@gmail.com>
2017-10-20 16:49:18 -0700

    bump version to 0.2 after some fixes

--- setup.py
+++ setup.py
@@ -100,7 +100,7 @@ entry_points = {'console_scripts': ['mope = mope.cli:main']}
 
 setup(
         name='mope',
-        version = '0.5',
+        version = '0.2',
         description='Molecular ontogenetic phylogeny estimation',
         author='Peter Wilton',
         author_email='pwilton@berkeley.edu',
