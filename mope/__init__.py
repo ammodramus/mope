6026f988cbdc39ceb9de59f242b5621b59106a41
Peter Wilton <peterrwilton@gmail.com>
2019-01-16 14:50:44 -0800

    bump version

--- mope/__init__.py
+++ mope/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 inf_data = None
 
-__version__ = '0.1.1'
+__version__ = '0.5.1'
 
 from .inference import Inference
 from .make_figures import make_figures
